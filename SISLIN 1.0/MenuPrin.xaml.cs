c8183d12e5f0641380a2ad3c94592b9339fb672a
Naiany Moreira França <naianymfranca@gmail.com>
2021-06-29 17:44:58 -0400

    Editei Tela Orçamento, tela ERRMovimento, tela perda

--- SISLIN 1.0/MenuPrin.xaml.cs
+++ SISLIN 1.0/MenuPrin.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using SISLIN_1._0.Relatorio;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -46,5 +47,7 @@ namespace SISLIN_1._0
             Tela_Orcamento SoVem = new Tela_Orcamento();
             SoVem.ShowDialog();
         }
+
+
     }
 }
