235645704297b64deaa1772639f38e7ba81152f9
Henrique Lopes <hl93482697@gmail.com>
2021-06-30 11:21:16 -0400

    Menu Inicial

--- SISLIN 1.0/MenuPrin.xaml.cs
+++ SISLIN 1.0/MenuPrin.xaml.cs
@@ -24,24 +24,11 @@ namespace SISLIN_1._0
         {
             InitializeComponent();
         }
-        private void Button_Click(object sender, RoutedEventArgs e)
-        {
-            Cadastrar_Promocao Chamar = new Cadastrar_Promocao();
-            Chamar.ShowDialog();
-        }
-
         private void Button_Click_1(object sender, RoutedEventArgs e)
         {
             Emitir_RRM1 Chamar = new Emitir_RRM1();
             Chamar.ShowDialog();
         }
-
-        private void Button_Click_2(object sender, RoutedEventArgs e)
-        {
-            Cadastrar_PerdasSimples Chamar = new Cadastrar_PerdasSimples();
-            Chamar.ShowDialog();
-        }
-
         private void Button_Click_3(object sender, RoutedEventArgs e)
         {
             Cadastrar_Orçamentos1 Chamar = new Cadastrar_Orçamentos1();
@@ -51,6 +38,18 @@ namespace SISLIN_1._0
         private void Button_Click_4(object sender, RoutedEventArgs e)
         {
             botaofeedback Chamar = new botaofeedback();
+            Chamar.ShowDialog();
+        }
+
+        private void Button_Click_2(object sender, RoutedEventArgs e)
+        {
+            Cadastrar_PerdasSimples Chamar = new Cadastrar_PerdasSimples();
+            Chamar.ShowDialog();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            Cadastrar_Promocao Chamar = new Cadastrar_Promocao();
             Chamar.ShowDialog();
         }
     }
