214a14534b3f40f542f79b593d03ef551f52a596
Gustavo <gustavo.saam.2020@gmail.com>
2021-10-13 14:30:13 -0400

    models,views, database, funcionariocs, funcionariodao, conexaocs
    gustavo

---
+++ SISLIN 1.0/Models/FuncionarioDAO.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SISLIN_1._0.Models
+{
+    class FuncionarioDAO
+    {
+    }
+}
