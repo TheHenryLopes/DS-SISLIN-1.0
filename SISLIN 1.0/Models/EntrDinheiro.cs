6765011f3596d6284b8bd0be299d3ab73cfad62f
Henrique Lopes <hl93482697@gmail.com>
2021-10-25 23:23:08 -0400

    EtrDinheiro e EntrDinheiroDAO

---
+++ SISLIN 1.0/Models/EntrDinheiro.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SISLIN_1._0.Models
+{
+    class EntrDinheiro
+    {
+        public int Id_Dinheiro { get; set; }
+        public double Valor_Dinheiro { get; set; }
+        public string Tipo_Dinheiro { get; set; }
+    }
+}
---
+++ SISLIN 1.0/Models/EntrDinheiroDAO.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SISLIN_1._0.Models
+{
+    class EntrDinheiroDAO
+    {
+        public void Delete(Funcionario t)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Funcionario GetById(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Insert(Funcionario t)
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<Funcionario> List()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Update(Funcionario t)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
