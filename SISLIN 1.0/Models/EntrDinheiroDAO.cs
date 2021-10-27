e3d04bb945f66646096cc5ff1b42a77166a62db4
naiany <naianymfranca@gmail.com>
2021-10-27 17:18:31 -0400

    Atualização

--- SISLIN 1.0/Models/EntrDinheiroDAO.cs
+++ SISLIN 1.0/Models/EntrDinheiroDAO.cs
@@ -5,6 +5,7 @@ using System.Text;
 using System.Threading.Tasks;
 using SISLIN_1._0.Interfaces;
 using SISLIN_1._0.Database;
+using MySql.Data.MySqlClient;
 
 namespace SISLIN_1._0.Models
 {
@@ -32,12 +33,12 @@ namespace SISLIN_1._0.Models
             try 
             {
                 var query = conc.Query();
-                query.CommandText = "INSERT INTO EntrDinheiro (id_dinheiro_func, valor_dinheiro_func, tipo_dinheiro_func)"
-                    + "VALUES (@id_dinheiro @valor_dinheiro @tipo_dinheiro)";
+                query.CommandText = "INSERT INTO Venda (id_venda, data_venda, valor_venda, formaPagamento_venda)"
+                    + "VALUES (null, @data, @valor @formPaga)";
 
-                query.Parameters.AddWithValue("@id_dinheiro", t.Id_Dinheiro);
-                query.Parameters.AddWithValue("@id_dinheiro", t.Valor_Dinheiro);
-                query.Parameters.AddWithValue("@id_dinheiro", t.Tipo_Dinheiro);
+                query.Parameters.AddWithValue("@data", t.Id_Dinheiro);
+                query.Parameters.AddWithValue("@valor", t.Valor_Dinheiro);
+                query.Parameters.AddWithValue("@formPaga", t.formapaga_Dinheiro);
 
                 var result = query.ExecuteNonQuery();
 
@@ -55,7 +56,35 @@ namespace SISLIN_1._0.Models
 
         public List<EntrDinheiro> List()
         {
-            throw new NotImplementedException();
+            try
+            {
+                List<EntrDinheiro> list = new List<EntrDinheiro>();
+
+                var query = conc.Query();
+                query.CommandText = "SELECT * FROM Venda";
+
+                MySqlDataReader reader = query.ExecuteReader();
+
+                while (reader.Read())
+                {
+                    list.Add(new EntrDinheiro()
+                    {
+                        Data = reader.GetDateTime("data_venda"),
+                        Valor_Dinheiro = reader.GetDouble("valor_venda"),
+                        formapaga_Dinheiro = reader.GetString("formaPagamento_venda")                     
+                    });
+                }
+
+                return list;
+            }
+            catch (Exception e)
+            {
+                throw e;
+            }
+            finally
+            {
+                conc.Close();
+            }
         }
 
         public void Update(EntrDinheiro t)
